39f52e3d7a7f03701909893f66caa47f62aaeafc
Roger Schönbächler <schoero@gmx.ch>
2020-07-06 16:00:48 +0200

    Wip

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    path: __dirname + "/lib/",
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: "awesome-typescript-loader"
+      }
+    ]
+  },
+  devServer: {
+    watchContentBase: true,
+    port: 8088
+  },
+};
